19c5fcd0d1bfe76f2ba946cf37f708783801e9f9
Metin Tabanlı Programlama Öğretimi <45932891+programlamaogretimi@users.noreply.github.com>
2019-03-16 19:02:26 +0300

    Add files via upload

---
+++ sarkiyazalim-fonksiyon-yok.py
@@ -0,0 +1,63 @@
+print('''
+Ali babanın bir çiftliği var
+Çiftliğinde inekleri var
+Möö möö diye bağırır
+Çiftliğinde Ali babanın
+
+Ali babanın bir çiftliği var
+Çiftliğinde kuzuları var
+Mee mee diye bağırır
+Çiftliğinde Ali babanın
+
+Ali babanın bir çiftliği var
+Çiftliğinde tavukları var
+Gıt gıt gıdak gıt gıt gıdak diye bağırır
+Çiftliğinde Ali babanın
+
+Ali babanın bir çiftliği var
+Çiftliğinde eşekleri var
+Ai ai ai ai diye bağırır
+Çiftliğinde Ali babanın
+
+Ali babanın bir çiftliği var
+Çiftliğinde köpekleri var
+Hav hav hav diye bağırır
+Çiftliğinde Ali babanın
+
+Ali babanın bir çiftliği var
+Çiftliğinde çocukları var
+Hah hah hah diye bağırır
+Çiftliğinde Ali babanın       
+''')
+
+
+print('Ali babanın bir çiftliği var')
+print('Çiftliğinde inekleri var')
+print('Möö möö diye bağırır')
+print('Çiftliğinde Ali babanın')
+print('')
+print('Ali babanın bir çiftliği var')
+print('Çiftliğinde kuzuları var')
+print('Mee mee diye bağırır')
+print('Çiftliğinde Ali babanın')
+print('')
+print('Ali babanın bir çiftliği var')
+print('Çiftliğinde tavukları var')
+print('Gıt gıt gıdak gıt gıt gıdak diye bağırır')
+print('Çiftliğinde Ali babanın')
+print('')
+print('Ali babanın bir çiftliği var')
+print('Çiftliğinde eşekleri var')
+print('Ai ai ai ai diye bağırır')
+print('Çiftliğinde Ali babanın')
+print('')
+print('Ali babanın bir çiftliği var')
+print('Çiftliğinde köpekleri var')
+print('Hav hav hav diye bağırır')
+print('Çiftliğinde Ali babanın')
+print('')
+print('Ali babanın bir çiftliği var')
+print('Çiftliğinde çocukları var')
+print('Hah hah hah diye bağırır')
+print('Çiftliğinde Ali babanın')      
+
