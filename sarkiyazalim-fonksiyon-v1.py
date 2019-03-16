79d4c77ae009c3950635921d5b55258293c0f12d
Metin Tabanlı Programlama Öğretimi <45932891+programlamaogretimi@users.noreply.github.com>
2019-03-16 19:02:47 +0300

    Add files via upload

---
+++ sarkiyazalim-fonksiyon-v1.py
@@ -0,0 +1,17 @@
+def aliBaba(canli, ses):
+    print('''
+Ali babanın bir çiftliği var
+Çiftliğinde %s var
+%s diye bağırır
+Çiftliğinde Ali babanın
+          ''' % (canli, ses))
+
+aliBaba('inekleri', 'Möö möö')
+aliBaba('kuzuları', 'Mee mee')
+aliBaba('tavukları', 'Gıt gıt gıdak gıt gıt gıdak')
+aliBaba('eşekleri', 'Ai ai ai ai')
+aliBaba('köpekleri', 'Hav hav hav')
+aliBaba('çocukları', 'Hah hah hah')
+
+
+
